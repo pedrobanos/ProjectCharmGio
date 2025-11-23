03f55ea7d682c610224353eadedd8e0212482e0a
Pedro Baños <pedrobanos.renault@gmail.com>
2025-11-23 11:43:02 +0100

    añadido precio a la vista Order

--- src/services/salesServices.js
+++ src/services/salesServices.js
@@ -162,6 +162,7 @@ export const getFilteredProducts = async ({
         nombre,
         cantidad,
         foto,
+        precio,
         lugar,
         sales: sales(product_id, cantidad)
       `,
